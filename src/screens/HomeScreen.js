6823da584c625145a03ef90d5146c30dfe4aa2e9
Admin <admin@Admins-MacBook-Pro.local>
2020-10-22 17:09:38 +0600

    design update

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+export default function HomeScreen() {
+
+  return (
+    <View style={{ flex: 1, paddingTop: 50}}>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <Text>Hello world!</Text>
+        <Text>Hello world!</Text>
+        <Entypo name="user" size={24} color="green" />
+        
+        <StatusBar style="auto" />
+      </View>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+
+      }}>
+        <Text style={styles.text2}>Apple</Text>
+        <Text style={styles.text2}>Banana</Text>
+      </View>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingTop: 30
+      }}>
+        <Text style={styles.text}>Row 1</Text>
+        <Text style={styles.text}>Row 2</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: { 
+    fontSize: 20, 
+    fontWeight: '600'
+  },
+  text2: { 
+    fontSize: 20, 
+    fontWeight: '600',
+    color: 'red'
+  }
+});
